15fea3507e25018ba46f7ff9ddc02be169c24a25
Johnnyc1423 <JohnEdChristensen@gmail.com>
2017-11-14 16:19:02 -0700

    Added some Unit tests for matrix Tools

--- PGComp/MatrixTools.py
+++ PGComp/MatrixTools.py
@@ -1,32 +1,31 @@
 import numpy
 import math
-def CalculateTransformations(transform, matrixList):
-	transform = numpy.transpose(transform)
-	newMatrixList = []
-	for matrix in matrixList:
-		transformedMatrix = numpy.dot(transform, matrix)
-		transformedMatrix = numpy.transpose(transformedMatrix)
-		newMatrixList.append(transformedMatrix)
-	return newMatrixList
+import os
+def CalculateTransformations(transform,basis):
+	basis = numpy.transpose(basis)
+	transformedMatrix = numpy.dot(transform,basis)
+	return numpy.transpose(transformedMatrix).tolist()
 def createMatrix(s1,s2,s3):
-	row1 = s1.split()
-	row1 = [float(i) for i in row1]
-	row2 = s2.split()
-	row2 = [float(i) for i in row2]
-	row3 = s3.split()
-	row3 = [float(i) for i in row3]
-	matrix = [row1,row2,row3]
-	return matrix
+    """
+    takes in 3 strings of 3 numbers seperated by commas
+    returns a 3x3 matrix
+    """
+    row1 = s1.split()
+    row1 = [float(i) for i in row1]
+    row2 = s2.split()
+    row2 = [float(i) for i in row2]
+    row3 = s3.split()
+    row3 = [float(i) for i in row3]
+    matrix = [row1,row2,row3]
+    return matrix	
 def detIsN(expectedDeterminent,m):
     """
         returns a 1 if nxn matrix m has a determinent equal to expectedDeterminent
         returns a zero otherwise
     """
     det = numpy.linalg.det(m)
-    if(det == expectedDeterminent):
-        return 1
-    else:
-        return 0
+    return(det == expectedDeterminent)
+
 def checkForDuplicates(m):
     """
         Determines if there are any duplicates in the list of matricies m
@@ -41,52 +40,46 @@ def checkForDuplicates(m):
             y = y + 1
         x = x + 1
 	
-def printMatrix(matrix):
-	print "-------------------------------"
-	print matrix
-	print "-------------------------------"
-	
 def saveMatrix(matrixIn,startPos,endPos,writeLocation):
-	matrixSave = matrixIn[startPos:endPos]
-	numpy.savetxt(writeLocation,numpy.vstack(matrixSave),fmt ='%-8f')
-	print "Saved " + str (len(matrixSave)) + " Matricies to " + writeLocation
+    matrixSave = matrixIn[startPos:endPos]
+    numpy.savetxt(writeLocation,numpy.vstack(matrixSave),fmt ='%-8f')
+    print "Saved " + str (len(matrixSave)) + " Matricies to " + writeLocation
 	
-def editStructEnum(matrix):
-	with open('struct_enum.in', 'r') as struct_enum:
-		structData = struct_enum.readlines()
-	structData[2] = str (matrix[0][0]) + "\t" + str (matrix[0][1]) + "\t" + str (matrix[0][2]) + "\n"
-	structData[3] = str (matrix[1][0]) + "\t" + str (matrix[1][1]) + "\t" + str (matrix[1][2]) + "\n"
-	structData[4] = str (matrix[2][0]) + "\t" + str (matrix[2][1]) + "\t" + str (matrix[2][2]) + "\n"
-	with open('struct_enum.in', 'w') as struct_enum:
-		struct_enum.writelines(structData)
-def readPGOut():
-	file = open("pgx_out.txt","r")
-	size = float (file.readline().split()[2])
-
-	if(size == 4):
-		size = 2
-	if (size == 8 or size == 16):
-		size = 3
+def editStructEnum(structPath,matrix):
+    with open(structPath, 'r') as struct_enum:
+        structData = struct_enum.readlines()
+    structData[2] = str (matrix[0][0]) + "\t" + str (matrix[0][1]) + "\t" + str (matrix[0][2]) + "\n"
+    structData[3] = str (matrix[1][0]) + "\t" + str (matrix[1][1]) + "\t" + str (matrix[1][2]) + "\n"
+    structData[4] = str (matrix[2][0]) + "\t" + str (matrix[2][1]) + "\t" + str (matrix[2][2]) + "\n"
+    with open(structPath, 'w') as struct_enum:
+        struct_enum.writelines(structData)
+def readPGOut(pgxPath):
+    file = open("pgxPath","r")
+    size = float (file.readline().split()[2])
+    if(size == 4 or size == 48):
+        size = 2
+    if (size == 8 or size == 16):
+        size = 3
 	
-	matrixList = []
+    matrixList = []
 
-	for i in range(0,int (size)):
-		file.readline()
-		row1 = file.readline()
-		row2 = file.readline()
-		row3 = file.readline()
-		matrixList.append(createMatrix(row1,row2,row3))
-	matrixList = matrixFloatToInt(matrixList)
-	file.close
-	return matrixList
+    for i in range(0,int (size)):
+        file.readline()
+        row1 = file.readline()
+        row2 = file.readline()
+        row3 = file.readline()
+        matrixList.append(createMatrix(row1,row2,row3))
+    matrixList = matrixFloatToInt(matrixList)
+    file.close
+    return matrixList
 
 def matrixFloatToInt(matrixList):
-	x = 0
-	y = 0
-	z = 0
-	for i in range(len(matrixList)):
-		for j in range(0,3):
-			for k in range(0,3):
-				matrixList[i][j][k] = int (round(matrixList[i][j][k],0))
-	return matrixList
-
+    for i in range(len(matrixList)):
+        for j in range(0,3):
+            for k in range(0,3):
+                matrixList[i][j][k] = int (round(matrixList[i][j][k],0))
+    return matrixList
+def generatePG(basis):
+    editStructEnum(basis)
+    os.system("../../kgrid_gen/pg.x > pgx_out.txt")
+    return readPGOut()
--- tests/test_MatrixTools.py
+++ tests/test_MatrixTools.py
@@ -1,7 +1,45 @@
-"""Tests The subroutines in PGComp/Matrixtools.py
+"""
+Tests The functions in PGComp/Matrixtools.py
 """
 import pytest
+import numpy
+import os
+from PGComp.MatrixTools import *
 def test_detIsN():
-	from PGComp.MatrixTools import detIsN
+    assert 1 == detIsN(1,[[1,0,0],[0,1,0],[0,0,1]])
+    assert 0 == detIsN(2,[[1,0,0],[0,1,0],[0,0,1]])
+    assert 1 == detIsN(2,[[2,0,0],[0,1,0],[0,0,1]])
+    
+def test_editStrucEnum():
+    editStructEnum("PGComp/struct_enum.in",[[1,0,0],[0,1,0],[0,0,1]])
+    with open('tests/IdentityTestStruct_enum.in', 'r') as struct_enum:
+        expectedStructData1 = struct_enum.readlines()
+    with open('PGComp/struct_enum.in', 'r') as struct_enum:
+        structData1 = struct_enum.readlines()
+    assert structData1 == expectedStructData1
+    
+    editStructEnum("PGComp/struct_enum.in",[[1.1,1.1,1],[1.1,1,1],[1.1,1,1]])
+    with open('tests/TestStruct_enum1.in', 'r') as struct_enum:
+        expectedStructData2 = struct_enum.readlines()
+    with open('PGComp/struct_enum.in', 'r') as struct_enum:
+        structData2 = struct_enum.readlines()
+    assert structData2 == expectedStructData2
 
-	assert 1 == detIsN(1,[[1,0,0],[0,1,0],[0,0,1]])
+def test_readPGOut():
+    editStructEnum("struct_enum.in",[[1,0,0],[0,1,0],[0,0,1]])
+    os.system("../kgrid_gen/pg.x > tests/pgx_out.txt")
+    with open('tests/TestPGOut1.txt', 'r') as PGOutData:
+        expectedData1 = PGOutData.readlines()
+    with open('tests/pgx_out.txt', 'r') as PGOutData:
+        Data1 = PGOutData.readlines()
+    assert expectedData1 == Data1
+def test_CalculateTransformations():
+    identity = [[1,0,0],[0,1,0],[0,0,1]]
+    m1 = [[1,2,3],[4,5,6],[7,8,9]]
+    transform1 = [[1,1,0],[1,1,0],[1,0,1]]
+    transform2 = [[1,1,5],[1,1,0],[1,5,1]]
+    assert numpy.array_equal(identity,CalculateTransformations(identity,identity))
+    assert numpy.array_equal(numpy.transpose(m1).tolist(),CalculateTransformations(m1,identity))
+    assert numpy.array_equal(m1,CalculateTransformations(identity,m1))
+    assert numpy.array_equal([[3,3,4],[9,9,10],[15,15,16]],CalculateTransformations(transform1,m1))
+    assert numpy.array_equal([[18,3,14],[39,9,35],[60,15,56]],CalculateTransformations(transform2,m1))
